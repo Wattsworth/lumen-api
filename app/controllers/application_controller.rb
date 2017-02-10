2e645090e7fc11f71cd49892f67691d8ff6e525d
John Donnal <jdonnal@mit.edu>
2017-02-09 23:38:10 -0500

    set up password recovery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@
 
 # application controller
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update,
+      keys: [:first_name, :last_name, :email])
+  end
 end
