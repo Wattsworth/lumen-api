3dcf44945ff104dbe3323228d4afd753e142725f
John Donnal <jdonnal@mit.edu>
2018-12-13 14:08:46 -0500

    bug fix in development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,16 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+      { :address => "localhost", :port => 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -51,4 +55,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+               :headers => :any,
+               :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+               :methods =>  [:get, :post, :options, :delete, :put]
+    end
+  end
+  
+  config.interface_url_template = lambda do |id|
+    #return "http://#{id}.interfaces.wattsworth.local"
+    return "http://wattsworth.local/api/interfaces/#{id}/"
+  end
 end
