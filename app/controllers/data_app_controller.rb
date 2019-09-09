eaf2a1d55a361ec16d0d6ac57b5270a7cb21a6e9
John Donnal <donnal@usna.edu>
2019-09-08 20:37:58 -0400

    removed unused code

--- app/controllers/data_app_controller.rb
+++ app/controllers/data_app_controller.rb
@@ -5,8 +5,8 @@ class DataAppController < ApplicationController
     @app = DataApp.find(params[:id])
     @nilm = @app.nilm
     head :unauthorized and return unless current_user.views_nilm?(@nilm)
-
-
+    # destroy any existing tokens
+    InterfaceAuthToken.where(user: current_user, data_app: @app).destroy_all
     token = InterfaceAuthToken.create(data_app: @app,
                                       user: current_user, expiration: 5.minutes.from_now)
     @auth_url = _app_auth_url(token)
@@ -19,15 +19,6 @@ class DataAppController < ApplicationController
 
     base = request.headers["HTTP_X_APP_BASE_URI"]
     "#{base}/#{token.data_app.id}/?auth_token=#{token.value}"
-  end
-
-  def authenticate_interface_user
-    @current_user = User.find_by_id(session[:user_id])
-    @app = DataApp.find_by_id(session[:interface_id])
-    if @current_user.nil? || @app.nil?
-      return false
-    end
-    true
   end
 
 end
--- app/views/nilms/show.json.jbuilder
+++ app/views/nilms/show.json.jbuilder
@@ -13,16 +13,11 @@ json.data do
       end
     end
   end
-  if @apps_available
-    json.data_apps(@nilm.data_apps) do |app|
-      json.id app.id
-      json.name app.name
-      json.url "#{@base_url}/#{app.id}/"
-      json.nilm_id @nilm.id
-    end
-  else
-    json.data_apps = []
+  json.data_apps(@nilm.data_apps) do |app|
+    json.id app.id
+    json.name app.name
+    json.url "" # this can be retrieved with data_app#show
+    json.nilm_id @nilm.id
   end
-
 end
 json.partial! 'helpers/messages', service: @service
