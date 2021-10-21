054c9415ae27f08fe810cf99493a530a0e1f6b54
John Donnal <jdonnal@gmail.com>
2021-10-20 21:43:07 -0400

    fixed migration bug

--- db/migrate/20160524023257_create_db_decimations.rb
+++ db/migrate/20160524023257_create_db_decimations.rb
@@ -1,11 +1,11 @@
 class CreateDbDecimations < ActiveRecord::Migration[5.0]
   def change
     create_table :db_decimations do |t|
-      t.integer :start_time, :int, :limit=>8
-      t.integer :end_time, :int, :limit=>8
-      t.integer :total_rows, :int, :limit=>8
-      t.integer :total_time, :int, :limit=>8
-      t.integer :db_file_id, :int
+      t.integer :start_time, :limit=>8
+      t.integer :end_time, :limit=>8
+      t.integer :total_rows, :limit=>8
+      t.integer :total_time, :limit=>8
+      t.integer :db_file_id
       t.timestamps null: false
     end
   end
--- db/migrate/20160709234451_remove_int_from_db_decimations.rb
+++ db/migrate/20160709234451_remove_int_from_db_decimations.rb
@@ -1,5 +1,6 @@
 class RemoveIntFromDbDecimations < ActiveRecord::Migration[5.0]
   def change
-    remove_column :db_decimations, :int
+    # fixed in 20160524023257_create_db_decimations
+    # remove_column :db_decimations, :int
   end
 end
